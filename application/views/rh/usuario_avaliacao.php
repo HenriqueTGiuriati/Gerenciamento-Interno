f82cbec736487f7f1936facb9861b075d95afe07
gustavohanaw <gustavo.henrique.barbosa@usp.br>
2016-05-31 12:33:22 -0300

    Update usuario_avaliacao.php

--- application/views/rh/usuario_avaliacao.php
+++ application/views/rh/usuario_avaliacao.php
@@ -1,1 +1,156 @@
-bbbbb
+<div class="box">
+            <div class="box-header with-border">
+              <h3 class="box-title">Avaliação Membros</h3>
+            </div>
+            <!-- /.box-header -->
+            <div class="box-body">
+              <table class="table table-bordered">
+                <tbody><tr>
+                  <th>Itens de Avaliação</th>
+                  <th style="width: 200px">Muito Bom</th>
+                  <th style="width: 200px">Bom</th>
+                  <th style="width: 200px">Regular</th>
+                  <th style="width: 200px">Insuficiente</th>
+                  
+                </tr>
+                <tr>
+                 <td>1.Assiduidade em reuniões e horário fixo</td>
+                 <td>   <div class="checkbox">
+                <label> 
+                <input type="checkbox" value=""></label>                  
+                 </div>
+                 </td>
+                 
+                 
+                 <td>    <div class="checkbox">
+                <label> 
+                <input type="checkbox" value=""></label>
+                  </div>
+                 </td>
+                 
+                 
+                 <td>   <div class="checkbox">
+                <label> 
+                <input type="checkbox" value=""></label>
+                  </div>
+                 </td>
+                 
+                 
+                 <td>   <div class="checkbox">
+                <label> 
+                <input type="checkbox" value=""></label>
+                  </div></td>               
+                </tr>
+                  <tr>
+                      
+                  <td>2.Pontualidade em reuniões</td>
+                  <td>   <div class="checkbox">
+                <label> 
+                <input type="checkbox" value=""></label>
+                  </div>
+                  </td>
+                 
+                  
+                  <td>   <div class="checkbox">
+                <label> 
+                <input type="checkbox" value=""></label>
+                  </div>
+                  </td>
+                  
+                  
+                  <td>   <div class="checkbox">
+                <label> 
+                <input type="checkbox" value=""></label>
+                  </div>
+                  </td>
+                  
+                  
+                  <td>   <div class="checkbox">
+                <label> 
+                <input type="checkbox" value=""></label>
+                  </div>
+                  </td>
+                 </tr>
+                
+                  <td>3.Participação em eventos (com chamadas)</td>
+                  <td>   <div class="checkbox">
+                <label> 
+                <input type="checkbox" value=""></label>
+                  </div>
+                  </td>
+                  
+                  
+                  <td>   <div class="checkbox">
+                <label> 
+                <input type="checkbox" value=""></label>
+                  </div>
+                  </td>
+                  
+                  
+                  <td>   <div class="checkbox">
+                <label> 
+                <input type="checkbox" value=""></label>
+                  </div>
+                  </td>
+                  
+                  
+                  <td>   <div class="checkbox">
+                <label> 
+                <input type="checkbox" value=""></label>
+                  </div>
+                  </td>
+                  <tr>
+                      
+                  <td>4.Participações em treinamentos</td>
+                  <td>
+                  <div class="checkbox">
+                <label> 
+                <input type="checkbox" value=""></label>
+                  </div>
+                  </td>
+                  
+                  
+                  <td>   
+                  <div class="checkbox">
+                <label> 
+                <input type="checkbox" value=""></label>
+                  </div>
+                  </td>
+                  
+                  
+                  <td>
+                <div class="checkbox">
+                <label> 
+                <input type="checkbox" value=""></label>
+                </div>
+                  </td>
+                  
+                  
+                  <td>
+                  <div class="checkbox">
+                <label> 
+                <input type="checkbox" value=""></label>
+                  </div>
+                  </td>
+                                
+                </tr>
+              </tbody></table>
+            </div>
+            <!-- /.box-body -->
+            <div class="box-footer clearfix">
+                  <td>
+                   <button type="button" style="width:150px class="btn btn-block btn-default">Enviar avaliações</button>
+                  </td>
+  
+          </div>
+          
+<script>              
+    
+    $(function () {
+        $('input').iCheck({
+          checkboxClass: 'icheckbox_square-green',
+          radioClass: 'iradio_square-green',
+          increaseArea: '20%' // optional
+        });
+      });
+    </script>
